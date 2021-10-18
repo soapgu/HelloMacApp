5dce702949bd04c061f006c35b2172e8169967f8
soapgu <ghost_gu@163.com>
2021-10-18 17:55:13 +0800

    Initial Commit

---
+++ HelloMacApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HelloMacApp
+//
+//  Created by guhui on 2021/10/18.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
