715d344b782fec4229dec088c5c415e0fe02d519
soapgu <ghost_gu@163.com>
2021-10-19 16:48:33 +0800

    first commit

--- HelloMacApp/ViewController.swift
+++ HelloMacApp/ViewController.swift
@@ -9,6 +9,17 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var nameField: NSTextField!
+    @IBOutlet weak var helloLabel: NSTextField!
+    @IBAction func sayButtonClicked(_ sender: Any) {
+        var name = nameField.stringValue
+        if name.isEmpty {
+          name = "World"
+        }
+        let greeting = "Hello \(name)!"
+        helloLabel.stringValue = greeting
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
